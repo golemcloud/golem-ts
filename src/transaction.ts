8c5c6eee65251f40ce9447496bb85446c5575cc6
Maxim Schuwalow <maxim.schuwalow@gmail.com>
2025-04-02 12:22:02 +0200

    fmt

--- src/transaction.ts
+++ src/transaction.ts
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { type OplogIndex, getOplogIndex, setOplogIndex } from "./hostapi";
 import { executeWithDrop, markAtomicOperation } from "./guard";
+import { type OplogIndex, getOplogIndex, setOplogIndex } from "./hostapi";
 import { Result } from "./result";
 
 /**
